1b92826cdc67ab9a529e4d242f223fec0a6a0d01
Michael Graute <m.graute@team-braeutigam.de>
2020-11-11 11:32:12 +0100

    - added properties to request

--- src/Pressmind/REST/Controller/AbstractController.php
+++ src/Pressmind/REST/Controller/AbstractController.php
@@ -38,6 +38,7 @@ abstract class AbstractController
         $apiTemplate = null;
         $limit = null;
         $order = null;
+        $properties = null;
         if(isset($parameters['readRelations'])) {
             $readRelations = boolval($parameters['readRelations']);
             unset($parameters['readRelations']);
@@ -51,9 +52,12 @@ abstract class AbstractController
             unset($parameters['start']);
             unset($parameters['limit']);
         }
-
+        if(isset($parameters['properties'])) {
+            $properties = $parameters['properties'];
+            unset($parameters['properties']);
+        }
         if(count($parameters) == 1 && isset($parameters['id'])) {
-            return $this->read($parameters['id'], $readRelations, $apiTemplate);
+            return $this->read($parameters['id'], $readRelations, $apiTemplate, $properties);
         }
         $this->orm_class->setReadRelations($readRelations);
         if(count($parameters) == 0) $parameters = null;
@@ -64,6 +68,18 @@ abstract class AbstractController
             }
             return $result;
         }
+        if(!is_null($properties) && is_array($properties)) {
+            $return = [];
+            $i=0;
+            foreach($this->orm_class->loadAll($parameters, $order, $limit) as $object) {
+                $return[$i] = [];
+                foreach ($properties as $property_name) {
+                    $return[$i][$property_name] = $object->$property_name;
+                }
+                $i++;
+            }
+            return $return;
+        }
         return $this->orm_class->loadAll($parameters, $order, $limit);
     }
 
@@ -71,16 +87,24 @@ abstract class AbstractController
      * @param $id
      * @param bool $readRelations
      * @param bool $apiTemplate
+     * @param array $properties
      * @return mixed|AbstractObject
      * @throws Exception
      */
-    public function read($id, $readRelations = false, $apiTemplate = false) {
+    public function read($id, $readRelations = false, $apiTemplate = false, $properties = null) {
         $this->orm_class->read($id);
+        $this->orm_class->setReadRelations($readRelations);
+        $this->orm_class->readRelations();
         if(!is_null($apiTemplate)) {
             return $this->orm_class->renderApiOutputTemplate($apiTemplate);
         }
-        $this->orm_class->setReadRelations($readRelations);
-        $this->orm_class->readRelations();
+        if(!is_null($properties) && is_array($properties)) {
+            $return = [];
+            foreach ($properties as $property_name) {
+                $return[$property_name] = $this->orm_class->$property_name;
+            }
+            return $return;
+        }
         return $this->orm_class;
     }
 }
